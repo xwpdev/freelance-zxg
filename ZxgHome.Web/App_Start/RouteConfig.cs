6b1aa870dc9e20da807f9d0cd84281a7849986d1
Xwpdev <charith.suriyakula@gmail.com>
2017-08-17 13:58:48 +0530

    content updates

--- ZxgHome.Web/App_Start/RouteConfig.cs
+++ ZxgHome.Web/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace ZxgHome.Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "NameRoute",
+                url: "{controller}/{action}/{name}",
+                defaults: new { controller = "Home", action = "Index", name = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
--- ZxgHome.Web/Controllers/HomeController.cs
+++ ZxgHome.Web/Controllers/HomeController.cs
@@ -7,7 +7,16 @@ namespace ZxgHome.Web.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            // ViewBag.EmailSuccess = false;
             return View();
+        }
+
+        // POST: Home/PostEmail
+        [HttpPost]
+        public ActionResult PostEmail(string name, string email, string message)
+        {
+            // ViewBag.EmailSuccess = true;
+            return View("Index");
         }
     }
 }
